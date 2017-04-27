77db3d62b9fa5490ec4420e3bdeb6b4beed9f233
Romain Quetiez <romain.quetiez@combodo.com>
2017-04-27 08:35:20 +0000

    Dico
    SVN:trunk[5948]

--- en.dict.combodo-archive-manual.php
+++ en.dict.combodo-archive-manual.php
@@ -4,4 +4,6 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Action:UnarchiveItem' => 'Unarchive!',
 	'Action:ArchiveList' => 'Archive all!',
 	'Action:UnarchiveList' => 'Unarchive all!',
+	'Msg:ArchivedSuccess' => 'The object has been successfully archived',
+	'Msg:UnarchivedSuccess' => 'The object has been successfully unarchived',
 ));
