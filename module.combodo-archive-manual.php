273e959d65ee47e2f45abeb67178e0de7c9a520a
Pierre Goiffon <pierre.goiffon@combodo.com>
2017-10-31 16:38:43 +0000

    combodo-archive-manual : preparing for release 1.0.1
    SVN:trunk[6237]

--- module.combodo-archive-manual.php
+++ module.combodo-archive-manual.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-archive-manual/1.0.0',
+	'combodo-archive-manual/1.0.1',
 	array(
 		// Identification
 		//
