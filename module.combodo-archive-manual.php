fa5f170194336a12023e4e0d7d9d4491da11a40a
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-06-26 07:26:44 +0000

    combodo-archive-manual : update module version
    SVN:trunk[6762]

--- module.combodo-archive-manual.php
+++ module.combodo-archive-manual.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-archive-manual/1.0.1',
+	'combodo-archive-manual/1.0.2',
 	array(
 		// Identification
 		//
@@ -26,7 +26,7 @@ SetupWebPage::AddModule(
 			'main.combodo-archive-manual.php'
 		),
 		'webservice' => array(
-			
+
 		),
 		'data.struct' => array(
 			// add your 'structure' definition XML files here,
@@ -34,11 +34,11 @@ SetupWebPage::AddModule(
 		'data.sample' => array(
 			// add your sample data XML files here,
 		),
-		
+
 		// Documentation
 		//
 		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
-		'doc.more_information' => '', // hyperlink to more information, if any 
+		'doc.more_information' => '', // hyperlink to more information, if any
 
 		// Default settings
 		//
